9f9aa1c238357b083bbb4e8610712cca33418516
Lance Miller <lellimecnar@gmail.com>
2017-09-04 03:30:16 +0000

    remove root index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const CardGame = require('./dist/CardGame.js');
-
-const game = new CardGame();
-
-game.addPlayer('Lance')
-	.startRound();
-
-game.currentRound.eachPlayer(player => player.deck.sort());
